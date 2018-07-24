761db1216275cbee430e3f8ca200caaac2404239
nilesh <nilesh111khankari@gmail.com>
2018-07-24 14:55:55 +0530

    adding app.js

---
+++ nil-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1 >Welcome to Nilesh</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
--- nil-react-app/src/index.js
+++ nil-react-app/src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<p>Hello ReactTestNil!!!</p>, document.getElementById('root'));
+//ReactDOM.render(<p>Hello ReactTestNil!!!</p>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<h1>TESt</h1>, document.getElementById('test'));
